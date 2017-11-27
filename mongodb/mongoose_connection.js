b6f858bd9101a471cad04a55893bc7c239242ff3
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 09:34:13 +0800

    "jackpot:  you may have an unnecessary change record" 		-- message from "diff"

--- mongodb/mongoose_connection.js
+++ mongodb/mongoose_connection.js
@@ -4,5 +4,9 @@ var url = 'mongodb://localhost/people';
 mongoose.connect(url,{useMongoClient: true});
 
 var contact= {
-    
+    email: String,
+    cell: String,
+    home: String
 }
+var contact_schema = new mongoose.Schema(contact);
+
