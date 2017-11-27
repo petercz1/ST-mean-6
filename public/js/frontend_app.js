02b4875d4805fd7334c26f528adfa2ae0857c29c
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 10:25:49 +0800

    Be careful when a loop exits to the same place from side and bottom.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,3 +1,4 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.
+var frontend_app = angular.module('employees',[]);
+frontend_app.controller('all_employees', do_all_employees);frontend_app.controller('single_employees', do_all_employees);
