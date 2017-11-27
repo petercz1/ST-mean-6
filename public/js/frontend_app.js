3af82c8a00a1f2e9aac1329a409d012b2a4d7a6f
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 10:28:03 +0800

    I'm a GENIUS!  I want to dispute sentence structure with SUSAN SONTAG!!

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,12 +1,16 @@
 console.log('loaded frontend app');
 
-var frontend_app = angular.module('employees',[]);
+var frontend_app = angular.module('employees', []);
 frontend_app.controller('all_employees', do_all_employees);
 
 function do_all_employees($scope, $http) {
-    
-    $scope.read = function () {
-        
-    }
+
+  $scope.read = function () {
+    console.log('reading all records');
+    $http.get('/api/v6/read')
+      .then(function (results) {
+          console.log(results);
+      });
+  }
 
 }
