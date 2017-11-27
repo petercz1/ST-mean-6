b2cf82a04f56b5516ea7aabd974a2d9d14c8b032
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 10:26:56 +0800

    This sentence no verb.

--- public/js/frontend_app.js
+++ public/js/frontend_app.js
@@ -1,4 +1,12 @@
 console.log('loaded frontend app');
 
 var frontend_app = angular.module('employees',[]);
-frontend_app.controller('all_employees', do_all_employees);frontend_app.controller('single_employees', do_all_employees);
+frontend_app.controller('all_employees', do_all_employees);
+
+function do_all_employees($scope, $http) {
+    
+    $scope.read = function () {
+        
+    }
+
+}
