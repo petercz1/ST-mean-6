cdf0e3b8a9fd3ee5cea09152c46207a1146ce236
petercz1 <email.peter.carroll@gmail.com>
2017-11-27 10:00:35 +0800

    For adult education nothing beats children.

--- mongodb/mongoose_connection.js
+++ mongodb/mongoose_connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var url = 'mongodb://localhost/people';
+var url = 'mongodb://localhost:27500/people';
 mongoose.connect(url,{useMongoClient: true});
 
 var contact= {
@@ -13,7 +13,7 @@ var contact_schema = new mongoose.Schema(contact);
 var employee = {
     name: String,
     gender: String,
-    contact: contact_schema
+    contact: contact
 }
 var employee_schema = new mongoose.Schema(employee);
 
